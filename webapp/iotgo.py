207156811acaf180e1c35159a466abdcc5ed9162
rizMehdi <ccmeall@gmail.com>
2021-07-30 22:50:35 +0200

    Update iotgo.py

--- webapp/iotgo.py
+++ webapp/iotgo.py
@@ -3,16 +3,16 @@ import streamlit.components.v1 as components
 st.set_page_config(page_title="IoTgo",page_icon=None,layout="wide")
 urlis="https://makecode.microbit.org/--docs?md=%0A%0A%60%60%60%20blocks%0Abasic.pause%281000%29%0Abasic.forever%28function%20%28%29%20%7B%0A%20%20%20%20if%20%28input.temperature%28%29%20%3E%3D%2028%29%7B%0A%20%20%20%20%20%20%20%20pins.digitalWritePin%28DigitalPin.P1%2C1%29%0A%20%20%20%20%20%20%20%20basic.pause%28100%29%0A%20%20%20%20%20%7D%20else%20%7B%0A%20%20%20%20%20%20%20%20pins.digitalWritePin%28DigitalPin.P1%2C0%29%0Abasic.pause%28100%29%0A%0A%0A%20%20%20%20%7D%0A%20%20%20%20if%20%28%21input.buttonIsPressed%28Button.A%29%29%7B%0A%20%20%20%20%20%20%20%20basic.showString%28%22Ciao%20from%20Bari%22%29%0A%20%20%20%20%20%20%20%20basic.pause%28100%29%0A%20%20%20%20%20%7D%20else%20%7B%0A%20%20%20%20%20%20%20%20basic.clearScreen%28%29%0Abasic.pause%28100%29%0A%0A%0A%20%20%20%20%7D%0A%7D%29%0A%60%60%60%0A%0A"
 
-cardWidth=140
-pluscardwidht=140
-missionCardWidth=175
-vertiPaddingWidth=50
+cardWidth=70
+pluscardwidht=70
+missionCardWidth=80
+vertiPaddingWidth=25
 
 # st.markdown("""""")
 applogo, empty1, empty2, mission, persona, empty3, thing, empty4,empty5  = st.beta_columns(9)
 
 with applogo:
-    st.image("http://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/applogo3.png",width=500)
+    st.image("http://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/applogo3.png",width=250)
 with mission:
     st.image("https://raw.githubusercontent.com/rizMehdi/IoTgo/main/images/cards/EN-mission-7.png", width=missionCardWidth)
 with persona:
@@ -52,7 +52,7 @@ with output_col:
 
 with code_col:
     # st.header("My code is:")
-    components.iframe(urlis,width=1100, height=1500, scrolling=True)
+    components.iframe(urlis,width=700, height=1500, scrolling=True)
 
 with emptycol5:
     st.button("Refresh (r)")
